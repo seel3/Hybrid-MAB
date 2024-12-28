78347c4f2c6baaa96c8d15ea6e3f629eaac257bf
seel3 <seel3@proton.me>
2024-12-28 12:59:24 +0100

    added GPU support for ember and malconv

--- prototype/minimal-prototype/models.py
+++ prototype/minimal-prototype/models.py
@@ -27,9 +27,17 @@ class MalConvModel(object):
             name (str, optional): _description_. Defaults to 'malconv'.
         """
         # TODO make sure the model is loaded correctly to GPU
-        self.model = MalConv(channels=256, window_size=512, embd_size=8).train()
-        weights = torch.load(model_path,map_location='cpu')
-        self.model.load_state_dict( weights['model_state_dict'])
+        
+        # Check if CUDA (GPU) is available
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        # Initialize model and move it to the device (GPU or CPU)
+        self.model = MalConv(channels=256, window_size=512, embd_size=8).to(self.device).train()
+
+        # Load weights
+        weights = torch.load(model_path, map_location=self.device)
+        self.model.load_state_dict(weights['model_state_dict'])
+        
         self.thresh = thresh
         self.__name__ = name
 
@@ -83,8 +91,12 @@ class EmberModel_2019(object):       # model in MLSEC 2019
             name (str, optional): name of the model. Defaults to 'ember'.
         """
         # TODO: make sure this is run on the GPU
+        if torch.cuda.is_available() :
+            self.model = lgb.Booster(model_file=model_path, params={'device': 'gpu'})
+        else:
+            self.model = lgb.Booster(model_file=model_path)
         # load lightgbm model
-        self.model = lgb.Booster(model_file=model_path)
+        #self.model = lgb.Booster(model_file=model_path)
         self.thresh = thresh
         self.__name__ = 'ember'
 
@@ -126,3 +138,60 @@ class ClamAV(object):
         else:
             print('clamav error')
             exit()
+
+
+# TODO: check if ember should be run like this
+
+#class EmberModel_gym(object):      # model in gym-malware
+#    # ember_threshold = 0.8336 # resulting in 1% FPR
+#    def __init__(self, model_path, thresh=0.9, name='ember'):       # 0.9 or 0.8336
+#        # load lightgbm model
+#        self.local_model = joblib.load(model_path)
+#        self.thresh = thresh
+#        self.__name__ = 'ember'
+#
+#    def get_score(self, file_path):
+#        with open(file_path, 'rb') as fp:
+#            bytez = fp.read()
+#            #return predict_sample(self.model, bytez) > self.thresh
+#            features = feature_extractor.extract( bytez )
+#            score = local_model.predict_proba( features.reshape(1,-1) )[0,-1]
+#            return score
+#    
+#    def is_evasive(self, file_path):
+#        score = self.get_score(file_path)
+#        return score < self.thresh
+
+
+#class EmberModel_2020(object):      # model in MLSEC 2020
+#    '''Implements predict(self, bytez)'''
+#    def __init__(self,
+#                 name: str = 'ember_MLSEC202H0',
+#                 thresh=0.8336):
+#        self.thresh = thresh
+#        self.__name__ = name
+#
+#    def get_score(self, file_path):
+#        with open(file_path, 'rb') as fp:
+#            bytez = fp.read()
+#            url = 'http://127.0.0.1:8080/'
+#            timeout = 5
+#            error_msg = None
+#            res = None
+#            start = time.time()
+#            try:
+#                res = self.get_raw_result(bytez, url, timeout)
+#                score = res.json()['score']
+#            except (requests.RequestException, KeyError, json.decoder.JSONDecodeError) as e:
+#                score = 1.0  # timeout or other error results in malicious
+#                error_msg = str(e)
+#                if res:
+#                    error_msg += f'-{res.text()}'
+#            return score
+#    
+#    def is_evasive(self, file_path):
+#        score = self.get_score(file_path)
+#        return score < self.thresh
+#    
+#    def get_raw_result(self, bytez, url, timeout):
+#        return requests.post(url, data=bytez, headers={'Content-Type': 'application/octet-stream'}, timeout=timeout)
